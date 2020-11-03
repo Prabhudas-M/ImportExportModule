8b56fff0b5f3a8d0709912be2b373420ee6b1d74
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-03 08:29:08 -0500

    all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as PI from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{PI.default}</li>
+    <li>{PI.double()}</li>
+    <li>{PI.Trible()}</li>
   </ul>,
   document.getElementById("root")
 );
---
+++ src/math.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const pi = 3.142345;
+
+function double() {
+  return pi * 2;
+}
+function Trible() {
+  return pi * 3;
+}
+export default pi;
+export { double, Trible };
